4965e3ec984bbcba27a0585fa6ebcc94bfd156dc
heedaelee <adguy72@gmail.com>
2022-03-25 17:14:24 +0900

    3/25

--- J2KB_Algorithm/DepthFirstSearch/Quiz/236.LowestCommonAncestor.js
+++ J2KB_Algorithm/DepthFirstSearch/Quiz/236.LowestCommonAncestor.js
@@ -32,13 +32,19 @@ const root = new Node(
   new Node(1, new Node(0, null, null), new Node(8, null, null))
 );
 
-console.log(root);
+//테스트를 위해 p,q를 root에서 해당 노드에 reference를 이렇게 갖고와서 사용 해야 한다.
+let p = root.left;
+let q = root.right;
+
+// console.log(root.left);
 function lowestCommonAncestor(node, p, q) {
-  if (!node || node === p || node === q) return node;
+  if (!node || node === p || node === q) {
+    return node;
+  }
   let resL = lowestCommonAncestor(node.left, p, q);
   let resR = lowestCommonAncestor(node.right, p, q);
   //L,R 자식 중 둘다 있으면  자신 리턴, 둘다 없으면 있는 자식 부분 node 리턴
-  return (resL && resR) ? node : (resL || resR);
+  return resL && resR ? node : resL || resR;
 }
 
-console.log(lowestCommonAncestor(root, p, q));
+console.log(lowestCommonAncestor(root, root.left, root.right));
