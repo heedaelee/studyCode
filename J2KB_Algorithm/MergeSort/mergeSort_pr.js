6052a54f2c7b5f63032fc585465986669fc121d0
heedaelee <adguy72@gmail.com>
2022-08-26 20:52:23 +0900

    완료: mergeSort.js

--- J2KB_Algorithm/MergeSort/mergeSort_pr.js
+++ J2KB_Algorithm/MergeSort/mergeSort_pr.js
@@ -1,8 +1,8 @@
-const num = 4;
-const array = [4, 3, 2, 1];
+// const array = [4, 3, 2, 1];
+const array = [4, 5, 6, 1, 2, 3, 8, 7];
 
-function mergeSort(block: Array) {
-  if (block.length === 1) return block[0];
+function mergeSort(block) {
+  if (block.length === 1) return block;
 
   const middle = Math.floor(block.length / 2);
 
@@ -12,11 +12,36 @@ function mergeSort(block: Array) {
   return merge(left, right);
 }
 
-function merge(params) {
+function merge(left, right) {
+  let i = 0;
+  let j = 0;
+  const newArray = [];
 
-  
+  // left, right 블록 첫번째 인덱스 부터 비교
+  while (left[i] && right[j]) {
+    if (left[i] <= right[j]) {
+      newArray.push(left[i]);
+      i++;
+    }
+    if (right[j] <= left[i]) {
+      newArray.push(right[j]);
+      j++;
+    }
+  }
 
+  //left 블록이 남아 있다면
+  while (left[i]) {
+    newArray.push(left[i]);
+    i++;
+  }
 
+  //right 블록이 남아 있다면
+  while (right[j]) {
+    newArray.push(right[j]);
+    j++;
+  }
+
+  return newArray;
 }
 
 console.log(mergeSort(array));
